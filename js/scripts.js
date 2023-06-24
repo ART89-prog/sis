51a542bb7f5e0ed0f49c1235431bf0a1623f2746
ART89-prog <dykpgilkom@mail.ru>
2023-06-24 22:31:28 +0300

    254464236432

---
+++ js/scripts.js
@@ -0,0 +1,123 @@
+$(() => {
+
+
+	// Ширина окна для ресайза
+	WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+	WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+	BODY = document.getElementsByTagName('body')[0]
+	OVERLAY = document.querySelector('.overlay')
+
+
+
+	$('body').on('click', '.modal_link', function (e) {
+		e.preventDefault()
+
+		Fancybox.close(true)
+		Fancybox.show([{
+			src: $(this).data('content'),
+			type: 'inline',
+		}]);
+	})
+
+
+	var swiper = new Swiper(".about_sertificat .swiper", {
+		slidesPerView: 3,
+		spaceBetween: 10,
+		loop: true,
+		allowTouchMove: false,
+
+		breakpoints: {
+			0: {
+				spaceBetween: 20,
+				slidesPerView: 3,
+				pagination: {
+					el: ".swiper-pagination",
+					clickable: true,
+				},
+				centeredSlides: true,
+				allowTouchMove: true
+			},			
+			767: {
+				spaceBetween: 20,
+				slidesPerView: 3
+			},
+			1023: {
+				spaceBetween: 10,
+				slidesPerView: 3
+			}
+		}
+	});
+
+
+
+	
+
+	// Fancybox
+	Fancybox.defaults.autoFocus = false
+	Fancybox.defaults.trapFocus = false
+	Fancybox.defaults.dragToClose = false
+	Fancybox.defaults.placeFocusBack = false
+	Fancybox.defaults.l10n = {
+		CLOSE: "Закрыть",
+		NEXT: "Следующий",
+		PREV: "Предыдущий",
+		MODAL: "Вы можете закрыть это модальное окно нажав клавишу ESC"
+	}
+
+	Fancybox.defaults.template = {
+		closeButton: '<img src=images/close2.svg>'
+	}
+
+
+	$('input[type=tel]').inputmask('+7 (999) 999-99-99')
+
+
+	// Скрол к пунктам меню
+	$(".scroll").on("click", function (e) {
+		e.preventDefault();
+		let id = $(this).attr("href");
+
+		$("html, body").animate({
+			scrollTop: $(id).offset().top
+		}, {
+			duration: 400,
+			easing: "swing"
+		});
+	});
+
+
+
+	window.addEventListener('resize', function () {
+		WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+
+		let windowW = window.outerWidth
+
+		if (typeof WW !== 'undefined' && WW != windowW) {
+
+
+			// Перезапись ширины окна
+			WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+
+
+			// Моб. версия
+			if (!fakeResize) {
+				fakeResize = true
+				fakeResize2 = false
+
+				document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
+			}
+
+			if (!fakeResize2) {
+				fakeResize2 = true
+
+				if (windowW < 375) document.getElementsByTagName('meta')['viewport'].content = 'width=375, user-scalable=no'
+			} else {
+				fakeResize = false
+				fakeResize2 = true
+			}
+		}
+	})
+
+
+
+})
